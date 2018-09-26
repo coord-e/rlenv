84340972990d9961312fb2f34fe7a1eac2a4fe16
coord.e <me@coord-e.com>
2018-09-26 18:48:36 +0900

    Add: Add bullet local installation section for roboschool

--- setup.sh
+++ setup.sh
@@ -25,6 +25,15 @@ add_and_checkout openai/roboschool $hash_roboschool
 
 cp rlenv/template/* .
 
+# Bullet installation for roboschool
+roboschool_path=$(realpath ./roboschool)
+mkdir -p bullet3/build
+pushd    bullet3/build
+cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
+make -j"$(nproc)"
+make install
+popd
+
 # Roboschool installation needs to be done in virtualenv
 pipenv run pipenv install
 
