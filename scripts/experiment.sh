d684c6363a41886724e61ed9aeaabd5a11421d1f
coord.e <me@coord-e.com>
2018-10-02 22:14:28 +0900

    Fix: Set tensorboard's logdir correctly in experiment

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -11,11 +11,13 @@ set_paths $name
 
 export OPENAI_LOG_FORMAT='log,csv,tensorboard'
 
-tensorboard --logdir "$log_dir" &
 for i in $(seq 5); do
-  export OPENAI_LOGDIR="$log_dir.$i"
+  LOG_PATH="$log_dir.$i"
+  tensorboard --logdir "$LOG_PATH" &
+  export OPENAI_LOGDIR="$LOG_PATH"
   echo "train seed=$i"
   time python ../run.py --seed $i --save_path "${model_path}_$i" --num_timesteps 2e6 $@
+  pkill tensorboard
 done
 
 trap 'pkill tensorboard' EXIT
