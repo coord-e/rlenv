1e6bb0fe4d3bd90fca7dd7b62562954210d7fe98
coord.e <me@coord-e.com>
2018-10-02 23:21:21 +0900

    Change: Make plot function

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,30 +6,36 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot(ax, name, is_detailed):
+    def load(path):
+        data = np.genfromtxt(path, dtype=float, delimiter=',', names=True)
+        return np.array([data['total_timesteps'], data['eprewmean']]).T
+
+    if is_detailed:
+        csvs = glob('results/{}/logs*/*.monitor.csv'.format(name))
+        data = [np.loadtxt(csv, skiprows=2, delimiter=',', usecols=(0,1)) for csv in csvs]
+        uselen = min(d.shape[0] for d in data)
+        data_shaped = np.array([d[:uselen] for d in data]).T
+
+        rewards = data_shaped[0]
+        timesteps = [t / 1e6 for t in np.mean(data_shaped[1], axis=1).cumsum()] # Show in (M)
+    else:
+        csvs = glob('results/{}/logs*/progress.csv'.format(name))
+        data = [load(csv) for csv in csvs]
+        uselen = min(d.shape[0] for d in data)
+        data_shaped = np.array([d[:uselen] for d in data]).T
+
+        timesteps = np.mean(data_shaped[0], axis=1) / 1e6 # Show in (M)
+        rewards = data_shaped[1]
+
+    rewards_mean = np.mean(rewards, axis=1)
+    rewards_std = np.std(rewards, axis=1)
+
+    ax.plot(timesteps, rewards_mean, color='blue', alpha=1)
+    ax.fill_between(timesteps, rewards_mean - rewards_std, rewards_mean + rewards_std, color="blue", alpha=1/3)
+
 name = sys.argv[1]
 is_detailed = sys.argv[2] == 'detailed' if len(sys.argv) > 2 else False
-
-def load(path):
-    data = np.genfromtxt(path, dtype=float, delimiter=',', names=True)
-    return np.array([data['total_timesteps'], data['eprewmean']]).T
-
-if is_detailed:
-    csvs = glob('results/{}/logs*/*.monitor.csv'.format(name))
-    data = [np.loadtxt(csv, skiprows=2, delimiter=',', usecols=(0,1)) for csv in csvs]
-    uselen = min(d.shape[0] for d in data)
-    data_shaped = np.array([d[:uselen] for d in data]).T
-
-    rewards = data_shaped[0]
-    timesteps = [t / 1e6 for t in np.mean(data_shaped[1], axis=1).cumsum()] # Show in (M)
-else:
-    csvs = glob('results/{}/logs*/progress.csv'.format(name))
-    data = [load(csv) for csv in csvs]
-    uselen = min(d.shape[0] for d in data)
-    data_shaped = np.array([d[:uselen] for d in data]).T
-
-    timesteps = np.mean(data_shaped[0], axis=1) / 1e6 # Show in (M)
-    rewards = data_shaped[1]
-
 
 fig = plt.figure(figsize=(12, 8))
 ax = fig.add_subplot(111)
@@ -39,11 +45,7 @@ ax.set_ylabel("Episodic Reward")
 
 ax.xaxis.set_major_formatter(plt.FormatStrFormatter('%.1f'))
 
-rewards_mean = np.mean(rewards, axis=1)
-rewards_std = np.std(rewards, axis=1)
-
-ax.plot(timesteps, rewards_mean, color='blue', alpha=1)
-ax.fill_between(timesteps, rewards_mean - rewards_std, rewards_mean + rewards_std, color="blue", alpha=1/3)
+plot(ax, name, is_detailed)
 
 plt.legend()
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@ cd "$(dirname $0)/.."
 source scripts/lib/paths.sh
 
 name=$1
+seed=${2:-1}
 shift
 
 set_paths $name
@@ -13,6 +14,6 @@ export OPENAI_LOG_FORMAT='stdout,log,csv,tensorboard'
 export OPENAI_LOGDIR="$log_dir"
 
 tensorboard --logdir "$log_dir" &
-python ../run.py --save_path "$model_path" $@
+python ../run.py --seed $seed --save_path "$model_path" $@
 
 trap 'pkill tensorboard' EXIT
