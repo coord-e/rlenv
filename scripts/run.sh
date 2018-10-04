4cfeef820f40a93fb730bf9670693b897b5280a9
coord.e <me@coord-e.com>
2018-10-05 08:58:59 +0900

    Add: Save cmdline and unstaged changes in output directory

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,17 @@ source scripts/lib/paths.sh
 name=$1
 shift
 
-set_paths $name
+cd ..
+tmp_diff=$(mktemp)
+git diff > $tmp_diff
+head=$(git rev-parse HEAD)
+patch_hash=$(sha1sum $tmp_diff | awk '{print $1}')
+cd rlenv
+
+set_paths $name-$head-$patch_hash
+cp $tmp_diff $output_dir/changes.diff
+
+echo "pipenv run train $name $@" > $output_dir/cmdline.txt
 
 export OPENAI_LOG_FORMAT='stdout,log,csv,tensorboard'
 export OPENAI_LOGDIR="$log_dir"
