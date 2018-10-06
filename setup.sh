51c1c6903e5086cb11e68093a9ed74dafde245ff
coord.e <me@coord-e.com>
2018-10-06 23:39:49 +0900

    Change: Use the copied script in boostraping

--- setup.sh
+++ setup.sh
@@ -45,25 +45,6 @@ reclone_submodule olegklimov/bullet3
 
 cp rlenv/template/* .
 
-# Bullet installation for roboschool
-roboschool_path=$(realpath ./roboschool)
-mkdir -p bullet3/build
-pushd    bullet3/build
-cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
-make -j"$(nproc)"
-make install
-popd
-
-if type "nvidia-smi" > /dev/null 2>&1
-then
-  sed -i 's/tensorflow/tensorflow-gpu/' Pipfile
-else
-  echo "Using tensorflow without GPU support"
-fi
-
-# Roboschool installation needs to be done in virtualenv
-pipenv install
-pipenv install -e ./baselines
-pipenv run pipenv install -e ./roboschool
+./setup.sh
 
 echo results/ >> .gitignore
