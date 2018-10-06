d8887edbd74ed6fa3608dbbbab860b0bfd280014
coord.e <me@coord-e.com>
2018-10-06 17:06:41 +0000

    Eject bootstrap script at c0cb16a1d0a8553321ad4f28c16250588dbd0f4e

--- setup.sh
+++ setup.sh
@@ -1,186 +1,24 @@
 #!/usr/bin/env bash
-readonly BD_EJECTED=true
-readonly BD_CACHE="$HOME/.cache/bd"
 
-if [ ! -d "$BD_CACHE" ]; then
-  mkdir -p $BD_CACHE
-fi
+set -eu
 
-if [ -v BD_EJECTED ]; then
-  BD_SCRIPT=$0
-else
-  BD_SCRIPT=$1
-  shift
-fi
-
-BD_ARGS=($@)
-BD_SCRIPT_NAME=$BD_SCRIPT
-
-bd_total_progress=0
-bd_current_progress=0
-
-BD_DEFAULT_EJECTED_FUNCTIONS=()
-
-BD_LOG_LEVEL=${BD_LOG_LEVEL:-info}
-name () 
-{ 
-    BD_SCRIPT_NAME=$1
-}
-description () 
-{ 
-    if [ -t 1 ]; then
-        BD_SCRIPT_DESCRIPTION=$(cat);
-    else
-        BD_SCRIPT_DESCRIPTION=$@;
-    fi
-}
-info () 
-{ 
-    log info "$@"
-}
-log () 
-{ 
-    local loglevel=$1;
-    shift;
-    local content=$@;
-    declare -A level2num=([debug]=0 [info]=1 [warn]=2 [error]=3);
-    if [ -z "${level2num[$BD_LOG_LEVEL]}" ]; then
-        local invalid_one=$BD_LOG_LEVEL;
-        BD_LOG_LEVEL=info;
-        bd::logger::error_exit "Invalid log level \"$invalid_one\"";
-    fi;
-    if [ ${level2num[$loglevel]} -lt ${level2num[$BD_LOG_LEVEL]} ]; then
-        return;
-    fi;
-    declare -A level2fmt=([debug]="\033[0;35m[DEBUG] \033[0m %s\n" [info]="\033[0;32m[INFO] \033[0m\033[0;01m %s\033[0;0m\n" [warn]="\033[0;33m[WARN] \033[0m\033[0;01m %s\033[0;0m\n" [error]="\033[0;31m[ERROR] \033[0m\033[0;01m %s\033[0;0m\n");
-    printf "${level2fmt[$loglevel]}" "$content" 1>&2
-}
-progress () 
-{ 
-    bd::store::load bd_current_progress bd_total_progress;
-    bd_current_progress=$((bd_current_progress += 1));
-    if [[ "$bd_total_progress" == "0" ]]; then
-        local status="$(printf "%3d" $bd_current_progress).";
-    else
-        local percentage=$(( bd_current_progress * 100 / bd_total_progress ));
-        local status="$(printf "%3d" $percentage)%";
-    fi;
-    local text="$1";
-    echo -e "\033[0;32m $status -> \033[0m\033[0;01m $text\033[0;0m";
-    if [[ $bd_current_progress -eq $bd_total_progress ]]; then
-        bd_current_progress=0;
-        bd_total_progress=0;
-    fi;
-    bd::store::save bd_total_progress bd_current_progress
-}
-bd::store::load () 
-{ 
-    for key in $@;
-    do
-        local path=$BD_CACHE/$key;
-        if [ -f $path.ary ]; then
-            path=$path.ary;
-        else
-            if [ ! -f $path ]; then
-                bd::logger::warn "loading variable \"$key\", which is not defined";
-            fi;
-        fi;
-        local value=$(cat $path);
-        if [[ "$path" == *.ary ]]; then
-            eval "$key=($value)";
-        else
-            eval "$key=\"$value\"";
-        fi;
-    done
-}
-bd::store::save () 
-{ 
-    for key in $@;
-    do
-        if [[ "$(declare -p $key)" =~ "declare -a" ]]; then
-            local value=$(eval "echo \"\${$key[@]}\"");
-            local path=$BD_CACHE/$key.ary;
-        else
-            if [ ! -v $key ]; then
-                bd::logger::warn "saving variable \"$key\", which is not defined";
-            else
-                local value=$(eval "echo \"\$$key\"");
-                local path=$BD_CACHE/$key;
-            fi;
-        fi;
-        echo "$value" > $path;
-    done
-}
-#!/usr/bin/env bd
-
-
-name "rlenv bootstrapping script"
-description << EOF
-Bootstrap the reinforcement learning project with rlenv.
-Usually, this script is executed directly from curl/wget.
-EOF
-
-
-function pushd () {
-  command pushd "$@" > /dev/null
-}
-
-function popd () {
-  command popd "$@" > /dev/null
-}
-
-info "rlenv bootstrapping script"
-
-progress "Initialize git and git-flow repository"
-
-git init
-git flow init
-
-progress "Clone rlenv as a submodule"
-
-git submodule add https://github.com/coord-e/rlenv
-pushd rlenv
-git submodule update --init --recursive
+# Bullet installation for roboschool
+roboschool_path=$(realpath ./roboschool)
+mkdir -p bullet3/build
+pushd    bullet3/build
+cmake -DBUILD_SHARED_LIBS=ON -DUSE_DOUBLE_PRECISION=1 -DCMAKE_INSTALL_PREFIX:PATH=$roboschool_path/roboschool/cpp-household/bullet_local_install -DBUILD_CPU_DEMOS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_EXTRAS=OFF  -DBUILD_UNIT_TESTS=OFF -DBUILD_CLSOCKET=OFF -DBUILD_ENET=OFF -DBUILD_OPENGL3_DEMOS=OFF ..
+make -j"$(nproc)"
+make install
 popd
 
-# Obtain the hash of baselines/roboschool in rlenv, and add them as a submodule
-function get_hash() {
-  pushd rlenv/$1
-  git rev-parse HEAD
-  popd
-}
+if type "nvidia-smi" > /dev/null 2>&1
+then
+  sed -i 's/tensorflow/tensorflow-gpu/' Pipfile
+else
+  echo "Using tensorflow without GPU support"
+fi
 
-function add_and_checkout() {
-  git submodule add https://github.com/$1
-  pushd $(basename $1)
-  git checkout $2
-  popd
-}
-
-function reclone_submodule() {
-  repo=$1
-  dir=$(basename $repo)
-  hash_rlenv=$(get_hash $dir)
-  add_and_checkout $repo $hash_rlenv
-}
-
-progress "Fetch submodules"
-
-reclone_submodule openai/baselines
-reclone_submodule openai/roboschool
-reclone_submodule olegklimov/bullet3
-
-progress "Copy template files"
-
-cp rlenv/template/* .
-
-progress "Install dependencies"
-
-./setup.sh
-
-progress "Commit changes"
-
-git add .
-git commit -m "Bootstrap with rlenv"
-
-info "Done!"
+# Roboschool installation needs to be done in virtualenv
+pipenv install
+pipenv install -e ./baselines
+pipenv run pipenv install -e ./roboschool
